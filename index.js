6466cbabc9eac8394cad4a384655e9ef2b5eac84
Rhutik Sahu <shubhamsahu938@gmail.com>
2022-08-07 03:36:12 +0200

    disabled feature added

--- index.js
+++ index.js
@@ -13,23 +13,37 @@ function addTodo (){
         alert('please enter some text in the field below')
     }
     else{
-        let todo2 = document.createElement('li');
-        let text2 = document.createTextNode(text)
-        // list.appendchild(todo)
-        // let todo = `${text}`
-        todo2.appendChild(text2)
-        todo2.classList.add('list-group-item')
-        list.append(todo2)
-        
-        
+        // let todo2 = document.createElement('li');
+        // let text2 = document.createTextNode(text)
+        // // list.appendchild(todo)
+        // // let todo = `${text}`
+        // todo2.appendChild(text2)
+        // todo2.classList.add('list-group-item')
+        // list.append(todo2)
+       const li = document.createElement('li')
+       list.append(li)
+       li.innerText = text;
+       li.classList.add('list-group-item')
+       li.onclick = function() {
+        if(li.classList.contains('disabled')){
+            li.classList.remove('disabled')
+        }
+        else{
+            li.classList.add('disabled')
+        }
+       }
     }
-    
-        
-    
-
-
 }
+
 submitbtn.addEventListener('click', (e)=>{
     e.preventDefault()
     addTodo()
+})
+
+list.addEventListener('click', function(e){
+
+    if (e.target && e.targetName == 'li'){
+        console.log(e.target.innerText + "was")
+    }
+
 })

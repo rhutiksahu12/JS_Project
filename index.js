649f5e33973544dd54ff631ec8adc277d7b2f747
Rhutik Sahu <shubhamsahu938@gmail.com>
2022-08-16 20:35:13 +0200

    roughly finalised

--- index.js
+++ index.js
@@ -1,49 +1,77 @@
-var list = document.querySelector('#list');
-var submitbtn = document.querySelector('#submit-btn')
-var inputtext = document.querySelector('#enterTodo');
-
+// var list = document.querySelector("#list");
+var submitbtn = document.querySelector("#submit-btn");
+var inputtext = document.querySelector("#enterTodo");
+let uid = 0;
+var removebtn;
+var editbtn;
 // console.log(list, submit, input)
 
-function addTodo (){
-    // e.preventDefault();
-    let text = inputtext.value;
-    console.log(text)
-    console.log('clicked')
-    if (text == ''){
-        alert('please enter some text in the field below')
-    }
-    else{
-        // let todo2 = document.createElement('li');
-        // let text2 = document.createTextNode(text)
-        // // list.appendchild(todo)
-        // // let todo = `${text}`
-        // todo2.appendChild(text2)
-        // todo2.classList.add('list-group-item')
-        // list.append(todo2)
-       const li = document.createElement('li')
-       list.append(li)
-       li.innerText = text;
-       li.classList.add('list-group-item')
-       li.onclick = function() {
-        if(li.classList.contains('disabled')){
-            li.classList.remove('disabled')
-        }
-        else{
-            li.classList.add('disabled')
-        }
-       }
-    }
+function addTodo() {
+  // e.preventDefault();
+  let text = inputtext.value;
+  console.log(text);
+  // console.log("clicked");
+  if (text == "") {
+    alert("please enter some text in the field below");
+  } else {
+    const li = document.createElement("li");
+    list.append(li);
+    li.innerText = text;
+    uid = uid + 1;
+    li.setAttribute("id", "uid");
+    li.prepend(uid);
+
+    removebtn = document.createElement("button");
+    removebtn.innerText = "Delete";
+    removebtn.setAttribute("type", "button");
+    removebtn.setAttribute("class", "btn btn-danger");
+
+    editbtn = document.createElement("button");
+    editbtn.innerText = "Edit";
+    editbtn.setAttribute("type", "button");
+    editbtn.setAttribute("class", "btn btn-primary");
+
+    li.append(removebtn);
+    li.append(editbtn);
+    removebtn.addEventListener("click", function (e) {
+      e.stopPropagation();
+      let target = e.target;
+      target.parentElement.remove();
+    });
+    editbtn.addEventListener("click", function (e) {
+      e.stopPropagation();
+      inputtext.value = e.target.parentElement.innerText;
+      console.log(text)
+    });
+
+    li.classList.add("list-group-item");
+    li.onclick = function () {
+      if (li.classList.contains("disabled")) {
+        li.classList.remove("disabled");
+      } else {
+        li.classList.add("disabled");
+      }
+    };
+  }
+  inputtext.value = ""
 }
 
-submitbtn.addEventListener('click', (e)=>{
-    e.preventDefault()
-    addTodo()
-})
+// addTodo();v
+submitbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addTodo();
+});
 
-list.addEventListener('click', function(e){
+// list.addEventListener("click", function (e) {
+//   if (e.target && e.targetName == "li") {
+//     console.log(e.target.innerText + "was");
+//   }
+// });
+// let deletebtn = document.getElementsByClassName("btn-danger");
+// console.log(removebtn)
 
-    if (e.target && e.targetName == 'li'){
-        console.log(e.target.innerText + "was")
-    }
+// removebtn.addEventListener("click", function () {
 
-})
+//     let li = document.getElementById(uid);
+//     li.remove();
+// });

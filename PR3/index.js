8519bc360bdb0b5b9791f73622b44a6641aba7c9
Rhutik Sahu <shubhamsahu938@gmail.com>
2022-08-24 14:41:52 +0200

    final pr3

--- PR3/index.js
+++ PR3/index.js
@@ -1,12 +1,24 @@
 var accordionOpen = document.querySelectorAll(".accordion");
 var moreinfo = document.querySelectorAll(".panel");
+var body = document.querySelector(".container");
 
 accordionOpen.forEach(function (panel, index) {
-    
-  panel.addEventListener("click", function () {
-    console.log(panel);
-    panel.classList.toggle("active");
+  panel.addEventListener("click", function (e) {
+    // console.log(panel);
+    // panel.classList.toggle("active");
+    moreinfo.forEach(function (info) {
+      info.classList.remove("active");
+    });
     moreinfo[index].classList.toggle("active");
-    console.log(moreinfo)
+    // console.log(moreinfo)
+    e.stopPropagation();
   });
+
+  
+
 });
+body.addEventListener("click", function (e) {
+  moreinfo.forEach(function (info) {
+    info.classList.remove("active");
+  });
+})
